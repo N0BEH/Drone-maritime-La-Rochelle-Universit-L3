4bbbdb47d7a2fd11942a031d0c889c50e7635147
vrassel <valentin.rassel@etudiant.univ-lr.fr>
2022-03-23 18:17:00 +0100

    Ajout des classes pour waypoint, route et nmea.

---
+++ app/src/main/java/com/example/projetsmartphone/NMEAConverter.kt
@@ -0,0 +1,14 @@
+package com.example.projetsmartphone
+
+/* RASSEL Valentin */
+//Class qui aura pour but de convertir des tram NMEA en données utilisable et inversement.
+
+class NMEAConverter {
+
+    //Classe Static ?
+
+    //NMEA --> Waypoint
+    //Waypoint --> NMEA
+
+
+}
---
+++ app/src/main/java/com/example/projetsmartphone/Waypoint.kt
@@ -0,0 +1,10 @@
+package com.example.projetsmartphone
+
+/* RASSEL Valentin */
+//Classe qui aura pour but d'être instancié pour enregistrement une coordonnée.
+
+class Waypoint {
+
+    //Latitude, Longitude, blablablah...
+
+}
