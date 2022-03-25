bfc6f1c2efc0b8cec607fb34fc2e22ec6a4e85f4
vrassel <valentin.rassel@etudiant.univ-lr.fr>
2022-03-25 18:07:02 +0100

    Modification de la classe waypoint

--- app/src/main/java/com/example/projetsmartphone/Waypoint.kt
+++ app/src/main/java/com/example/projetsmartphone/Waypoint.kt
@@ -3,8 +3,25 @@ package com.example.projetsmartphone
 /* RASSEL Valentin */
 //Classe qui aura pour but d'être instancié pour enregistrement une coordonnée.
 
-class Waypoint {
+class Waypoint(lat : String, long : String, indiLat : String, indiLong : String, h : String)
+{
+    val latitude: String = lat
+    val longitude: String = long
 
-    //Latitude, Longitude, blablablah...
+    val heure: String = h
+
+    //NORTH / SOUTH
+    val indicateurLatitude: String = indiLat
+    //EAST / WEST
+    val indicateurLongitude: String = indiLong
+
+    /*
+    pour test
+    val wp = Waypoint("4618.070", "00109.482", "N", "W", "155816.881")
+
+        println("---")
+        println("${wp.heure}")
+        println("---")
+     */
 
 }
