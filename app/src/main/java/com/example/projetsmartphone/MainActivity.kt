6ada6bb822977fd12302b65827310ebc9884d018
Evan-POTIN <evanptn36@gmail.com>
2022-03-30 14:36:57 +0200

    Connection client fonctionnel

--- app/src/main/java/com/example/projetsmartphone/MainActivity.kt
+++ app/src/main/java/com/example/projetsmartphone/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.projetsmartphone
 
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.net.InetSocketAddress
 import java.net.Socket
-import androidx.appcompat.app.AppCompatActivity
-
 
 
 open class MainActivity : AppCompatActivity() {
@@ -15,14 +15,24 @@ open class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val adresse : String = "0.0.0.0"
-        val port : Int = 9000
-        val client = Socket(adresse, port)
-        val input = BufferedReader(InputStreamReader(client.inputStream))
-        println("Client receiving [${input.readLine()}]")
-
-
+        val thread = Thread {
+            try {
+                val client = Socket()
+                val socketAddress : InetSocketAddress = InetSocketAddress("10.13.8.197", 9000)
+                try{
+                    client.connect(socketAddress)
+                    val txt = BufferedReader(InputStreamReader(client.inputStream)).readLine()
+                    println("Le client recoit $txt")
+                }catch(e : Exception){
+                    println("aaaaaa")
+                    client.close()
+                    println("Exception caught:$e")
+                }
+            } catch (e: java.lang.Exception) {
+                e.printStackTrace()
+            }
+        }
+        thread.start()
     }
 }
 
